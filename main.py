272a89780e8d0b0727de611a0e6cfeac6e40ea1b
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-30 12:51:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ import os
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--niters', type=int, default=5000, help='Number of training iterations')
+	parser.add_argument('--niters', type=int, default=1500, help='Number of training iterations')
 	parser.add_argument('--batch_size', type=int, default=16, help='Batch size')
 	parser.add_argument('--filterG', type=int, default=64, help='Number of filters G')
 	parser.add_argument('--filterD', type=int, default=64, help='Number of filters D')
 	parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate imG')
-	parser.add_argument('--res', type=bool, default=False, help='Use ResNet Assessor')
 	parser.add_argument('--data_path', type=str, default='../Data/4dct_clean',help='Path to data.')
 	parser.add_argument('--ngpu', type=int, default=2, help='Number of GPUs')
 	parser.add_argument('--steps_per_log', type=int, default=10, help='Output Iterations')
